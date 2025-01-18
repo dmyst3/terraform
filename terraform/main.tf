44a9e19d2ab993722f8ff856ab55eadb36a3e912
Ahmad Bacchus <dmyst3@yahoo.com>
2025-01-18 15:27:08 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,14 @@
-$ cat <<EOT >> hello.tf
-resource "null_resource" "default" {
-  provisioner "local-exec" {
-    command = "echo 'Hello World'"
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-25488752"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.web.id}"]
+  user_data              = "${file("template/user_data.sh")}"
+
+  tags {
+    Name = "hello-world-web"
   }
 }
-EOT
