5b9922eef98c294a1055caa6924dcddd51c99134
Ahmad Bacchus <dmyst3@yahoo.com>
2025-01-24 12:39:26 -0500

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "cost_center" {
+  description = "Cost Center"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name"
+  type        = string
+}
+
+variable "iam" {
+  description = "iam"
+  type        = string
+}
+
+variable "owner" {
+  description = "Owner"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
